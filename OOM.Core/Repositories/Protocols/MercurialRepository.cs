52f7e53c5fd6b5a91d1610c862651766ca8c7c5a
Luan Mattner Müller <luanmm@gmail.com>
2014-12-14 23:25:05 -0200

    Mercurial repository connection class.

---
+++ OOM.Core/Repositories/Protocols/MercurialRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Mercurial;
+
+namespace OOM.Core.Repositories.Protocols
+{
+    public class MercurialRepository : Repository
+    {
+        private Mercurial.Repository _repository;
+
+        public MercurialRepository(RepositoryConfiguration configuration)
+            : base(configuration)
+        {
+            //_repository = new Mercurial.Repository(LocalPath);
+        }
+
+        public override bool Update()
+        {
+            return true;
+        }
+    }
+}
